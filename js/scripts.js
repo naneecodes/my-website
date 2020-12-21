b585e07be288d9125954322a56e18a19ef2e5049
nannabi <nanee.codes@gmail.com>
2020-12-21 17:13:31 +0900

    adjust menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
-// HAMBURGER SLIDE ANIMATION
+//GLOBAL VARIABLES
+
+// NAV SLIDE ANIMATION
 const menuSlide = () => {
   const burger = document.querySelector(".burger");
   const menu = document.querySelector(".menu-links");
+  const topLine = document.querySelector(".line1");
+  const middleLine = document.querySelector(".line2");
+  const bottomLine = document.querySelector(".line3");
 
   burger.addEventListener("click", () => {
+    //show 'x' toggle
+    middleLine.classList.toggle("menu-open-middle");
+    topLine.classList.toggle("menu-open-top");
+    bottomLine.classList.toggle("menu-open-bottom");
+
+    // nav bar slides open to the left
     menu.classList.toggle("nav-active");
   });
 };
